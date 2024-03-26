3cc4f7b04922598877a3a7d6cf84f4128ae12904
jerry-hong7 <jerry.hong@emory.edu>
2024-03-25 22:08:05 -0400

    Finalized script and report, added figures

--- code/02_analyze.R
+++ code/02_analyze.R
@@ -1,5 +1,5 @@
 # Author: Jerry Hong
-# Disclaimer: Some or all the code below is through the assistance of AI generated sources like GitHub Copilot
+# Disclaimer: Some or all the code below is through the assistance of AI generated sources like GitHub Copilot as well as outside internet resources
 
 # load libraries
 library(tidyverse)
@@ -43,6 +43,16 @@ penn_subset %>%
             sd_log_duration = sd(log_duration, na.rm = TRUE)) %>%
   write.csv('tables/tg_summary.csv')
 
+# count table
+penn_subset %>%
+  group_by(tg) %>%
+  summarize(female_count = sum(female),
+            black_count = sum(black),
+            othrace_count = sum(othrace),
+            agelt35_count = sum(agelt35),
+            agegt54_count = sum(agegt54)) %>%
+  write.csv('tables/tg_count.csv')
+
 # prop table for female
 prop_female <- with(penn_subset, prop.table(table(tg, female), margin =2))
 
@@ -59,7 +69,6 @@ prop_agelt35 <- with(penn_subset, prop.table(table(tg, agelt35), margin =2))
 prop_agegt54 <- with(penn_subset, prop.table(table(tg, agegt54), margin =2))
 
 prop_df <- cbind(prop_female, prop_black, prop_othrace, prop_agelt35, prop_agegt54)
-prop_df
 
 colnames(prop_df) <- c("Male", "Female",
                                     "Not Black", "Black",
@@ -68,8 +77,22 @@ colnames(prop_df) <- c("Male", "Female",
                                     "Not Older 54", "Older 54")
 row.names(prop_df) <- c("Control", "Treatment")
 
+write.csv(prop_df, 'tables/tg_prop.csv')
+
 # with each proportion table, the control has a slight majority in 
 # all categories except being other race (othrace) where the 57% are in the treatment group
+
+# plotting the two unemployment stints and save the plot
+penn_subset %>%
+  ggplot(aes(x = log_duration, fill = factor(tg))) +
+  geom_density(alpha = 0.5) +
+  labs(title = "Density plot of log duration by treatment group",
+       x = "Log duration (weeks)",
+       y = "Density") +
+  scale_fill_discrete(name="Treatment Group",
+                       labels=c("Control", "Treatment")) +
+  theme_minimal() +
+  ggsave('plots/log_duration_density.png', width = 8, height = 6)
 
 # estimating average treatment effect on log_duration
 summary(estimatr::lm_robust(log_duration ~ tg, data = penn_subset))
@@ -87,20 +110,20 @@ penn_subset <- penn_subset %>%
   mutate(btw35_54 = ifelse(agelt35 == 0 & agegt54 == 0, 1, 0))
 
 # ATE for the three race groups
-summary(estimatr::lm_robust(log_duration ~ tg * white, data = penn_subset))
-summary(estimatr::lm_robust(log_duration ~ tg * black, data = penn_subset))
-summary(estimatr::lm_robust(log_duration ~ tg * othrace, data = penn_subset))
+summary(estimatr::lm_robust(log_duration ~ tg + white, data = penn_subset))
+summary(estimatr::lm_robust(log_duration ~ tg + black, data = penn_subset))
+summary(estimatr::lm_robust(log_duration ~ tg + othrace, data = penn_subset))
 
 # ATE variation for race, setting othrace as reference
-summary(estimatr::lm_robust(log_duration ~ tg * white + tg * black, data = penn_subset))
+summary(estimatr::lm_robust(log_duration ~ tg*(white+black), data = penn_subset))
 
 # ATE for the three age groups
-summary(estimatr::lm_robust(log_duration ~ tg * agelt35, data = penn_subset))
-summary(estimatr::lm_robust(log_duration ~ tg * agegt54, data = penn_subset))
-summary(estimatr::lm_robust(log_duration ~ tg * btw35_54, data = penn_subset))
+summary(estimatr::lm_robust(log_duration ~ tg + agelt35, data = penn_subset))
+summary(estimatr::lm_robust(log_duration ~ tg + agegt54, data = penn_subset))
+summary(estimatr::lm_robust(log_duration ~ tg + btw35_54, data = penn_subset))
 
 # ATE variation for age, setting btw35_54 as reference
-summary(estimatr::lm_robust(log_duration ~ tg * agelt35 + tg * agegt54, data = penn_subset))
+summary(estimatr::lm_robust(log_duration ~ tg*(agelt35+agegt54), data = penn_subset))
 
-# ATE variation for gender and race
-summary(estimatr::lm_robust(log_duration ~ tg * agelt35 + tg * white, data = penn_subset))
+# ATE variation for gender and race, setting othrace as reference
+summary(estimatr::lm_robust(log_duration ~ tg + female*(white+black), data = penn_subset))
